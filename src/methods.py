20a8afb037983ba7cbe42ba9aadccb3c994696fc
김영욱 <ziziza93@naver.com>
2021-12-07 01:41:17 +0900

    코인 자동거래봇 수정, 매수로직 작성

--- src/methods.py
+++ src/methods.py
@@ -1,6 +1,19 @@
+# -*- coding:utf-8 -*-
 import time
 import pyupbit
 import pandas as pd
+
+
+# 프린트 메소드
+def log_print(message):
+    now = time.localtime()
+    now = "[%04d/%02d/%02d %02d:%02d:%02d]" % (now.tm_year, now.tm_mon, now.tm_mday, now.tm_hour, now.tm_min, now.tm_sec)
+    print(now, message)
+
+
+# 일반 api 호출 시간지연 메소드
+def delay_for_normal_api():
+    time.sleep(0.07)
 
 
 # 거래대금이 많은 순으로 코인 리스트를 얻는다.
@@ -8,24 +21,28 @@ import pandas as pd
 # top : 상위 몇개를 확인할것인지 지정
 def get_top_coin_list(interval, top):
     # 원화 마켓의 코인 티커를 리스트로 담아요.
-    tickers = pyupbit.get_tickers("KRW")
+    tickers = pyupbit.get_tickers("KRW", True, False, True)
+    delay_for_normal_api()
 
     # 딕셔너리를 하나 만듭니다.
     dic_coin_money = dict()
 
     # for문을 돌면서 모든 코인들을 순회합니다.
     for ticker in tickers:
+        if ticker["market_warning"] != "NONE":
+            log_print(ticker["market"] + "은 정상적인 상태가 아닙니다 목록에서 제외합니다.")
+            continue
         try:
             # 캔들 정보를 가져온다.
-            df = pyupbit.get_ohlcv(ticker, interval)
+            df = pyupbit.get_ohlcv(ticker["market"], interval)
             # 최근 2개 봉의 거래대금을 합산한다
             volume_money = float(df['value'][-2]) + float(df['value'][-1])
             # 이걸 위에서 만든 딕셔너리에 넣어줍니다. Key는 코인의 티커, Value는 위에서 구한 거래대금
-            dic_coin_money[ticker] = volume_money
+            dic_coin_money[ticker["market"]] = volume_money
             # 티커와 거래액을 출력해 봅니다.
-            # print(ticker, dic_coin_money[ticker])
+            # print(ticker["market"], dic_coin_money[ticker["market"]])
             # api 최대 호출 횟수 피하기 위한 시간텀
-            time.sleep(0.07)
+            delay_for_normal_api()
 
         except Exception as e:
             print("exception:", e)
@@ -50,7 +67,6 @@ def get_top_coin_list(interval, top):
             coin_list.append(coin_data[0])
         else:
             break
-
     # 코인 리스트를 리턴해 줍니다.
     return coin_list
 
@@ -71,19 +87,112 @@ def get_rsi(ohlcv, period, st):
     return float(pd.Series(100 - (100 / (1 + rs)), name="RSI").iloc[st])
 
 
-# 티커에 해당하는 코인이 매수된 상태면 참을 리턴하는함수
-def is_has_coin(balances, ticker):
-    has_coin = False
-    for value in balances:
-        real_ticker = value['unit_currency'] + "-" + value['currency']
-        if ticker == real_ticker:
-            has_coin = True
-    return has_coin
+# 내가 소유한 코인 목록을 받아오는 메소드
+def get_my_coins(upbit):
+    balances = upbit.get_balances()
+    delay_for_normal_api()
+    my_coins = list()
+    for balance in balances:
+        if balance["currency"] == "KRW" or float(balance["avg_buy_price"]) == float(0):
+            continue
+        my_coin = "KRW-" + balance["currency"]
+        my_coins.append(my_coin)
+    return my_coins
 
 
-# 프린트 메소드
-def log_print(message):
-    now = time.localtime()
-    now = "[%04d/%02d/%02d %02d:%02d:%02d]" % (now.tm_year, now.tm_mon, now.tm_mday, now.tm_hour, now.tm_min, now.tm_sec)
-    print(now, message)
+# 시장상황 분석 메소드 (하락장, 상승장, 일반장 구분)
+def check_market_status(target_coin):
+    df_60 = pyupbit.get_ohlcv(target_coin, "minute60")  # 60분봉 정보
+    delay_for_normal_api()
+    rsi_14_before = get_rsi(df_60, 14, -4)              # 3시간 전 rsi14 지표
+    rsi_14_after = get_rsi(df_60, 14, -2)               # 1시간 전 rsi14 지표
+    condition = rsi_14_after / rsi_14_before            # 1시간 전 지표 / 3시간 전 지표(장 상황 체크기준)
+    market_status = "일반장"                             # 장 상태(일반장)
+    if condition >= 1.1:
+        market_status = "상승장"                         # 장 상태(상승장) 체크기준이 1.1 이상인 경우
+    elif condition < 0.95:
+        market_status = "하락장"                         # 장 상태(하락장) 체크기준이 0.95 이하인 경우
 
+    return market_status
+
+
+# 목표 수익율 지정 메소드
+def select_revenue_rate(market_status):
+    target_revenue = 1.02
+    if market_status == "일반장":
+        target_revenue = 1.02
+    elif market_status == "상승장":
+        target_revenue = 1.03
+    elif market_status == "하락장":
+        target_revenue = 1.01
+
+    return target_revenue
+
+
+# 타겟코인 시장분석 후 매도하는 로직
+# upbit : 로그인 된 업비트 객체
+# target_coin : 매도할 코인 티커
+# invest_balance : 투자 원금
+# now_balance : 투자금 잔액
+def sell_logic(upbit, target_coin, invest_balance, now_balance):
+    log_print("매도로직을 시작합니다.")
+    log_print("거래 대상: " + str(target_coin))
+
+    # 시장상황 분석
+    market_status = check_market_status(target_coin)
+
+    # 목표 수익율 설정
+    target_revenue = select_revenue_rate(market_status)
+
+    log_print("시장 상황: " + market_status + ", 목표 수익율: " + str(round((target_revenue - 1.0) * 100, 1)) + '%')
+
+    # 내가 보유한 코인중 타겟 코인의 정보를 가져온다.
+    my_coin = ""
+    for balance in upbit.get_balances():
+        if "KRW-" + balance["currency"] == target_coin:
+            my_coin = balance
+    delay_for_normal_api()
+
+    # 투자금 기준 평단가를 계산한다. (투자원금 - 현재잔고) / 코인 갯수
+    avg_buy_price = (invest_balance - now_balance) / float(my_coin["balance"])
+
+    # 투자금 기준 평단가가 업비트에 기재된 평단가보다 낮은 경우
+    if avg_buy_price < float(my_coin["avg_buy_price"]):
+        # 평단가는 업비트에 기재된 평단가를 기준으로 한다.
+        avg_buy_price = float(my_coin["avg_buy_price"])
+
+    # 목표 가격을 정하고 목표 가격보다 현재 가격이 높은경우 매도한다.
+    target_price = avg_buy_price * target_revenue
+    now_price = pyupbit.get_current_price(target_coin)
+    if now_price >= target_price:
+        upbit.sell_market_order(target_coin, float(my_coin["balance"]))
+        log_print("코인을 모두 판매했습니다.")
+        delay_for_normal_api()
+        return True
+    else:
+        log_print("수익률에 도달하지 못했습니다.")
+        delay_for_normal_api()
+        return False
+
+
+# 매수 대상인지 체크하는 메소드
+def check_purchase_target():
+    # 로직 고도화 시켜야함
+    is_purchase_target = False
+    # 매수 대상이 맞다면
+    if is_purchase_target:
+        return True
+    # 매수 대상이 아니라면
+    else:
+        return False
+
+
+# 대상코인 매수 함수
+def buy_target_coin(ticker):
+    print("구매한당")
+
+
+# 매수로직
+def buy_logic():
+    print("매수로직은 매도로직도 포함해야하고 매수대상 체크도 해야하고 실제 매수함수도 포함해야한다")
+    print("조건은 트레이딩봇에 넣어놓았다")
--- src/upbit_trading_bot.py
+++ src/upbit_trading_bot.py
@@ -10,13 +10,14 @@ secret = "BNwix0tO4IPIn4GzWcxqVnRxVxItxJPh4sm3DYj5"  # 본인 값으로 변경
 # 업비트 객체를 만듭니다.
 upbit = pyupbit.Upbit(access, secret)
 
+
 while True:
     methods.log_print("투자 봇 가동")
     # 내 원화 잔고를 가져온다
     my_balance = int(upbit.get_balance("KRW"))
 
     # 투자금액 지정
-    invest_rate = 0.1                               # 자산의 10%만 투자한다
+    invest_rate = 0.2                               # 자산의 20%만 투자한다
     invest_min = 100000                             # 최소 10만원 이상 투자한다
     invest_balance = int(my_balance * invest_rate)  # 투자금 계산
     if invest_balance < invest_min:                 # 투자금이 최소 투자금보다 작은경우
@@ -24,141 +25,171 @@ while True:
             invest_balance = my_balance             # 투자금 = 원금 전체
         else:                                       # 투자원금이 최소 투자금보다 큰경우
             invest_balance = invest_min             # 투자금 = 최소 투자금
+    now_balance = invest_balance
     methods.log_print("투자금 지정 완료: " + str(invest_balance) + "원")
 
-    # 투자대상 선정(1시간봉 기준 2개의 거래량 1등)
-    methods.log_print("투자 대상 선정 시작")
-    target_coins = methods.get_top_coin_list("minute60", 10)
+    # 투자대상 선정(1시간봉 기준 2개의 거래량 상위 or 보유코인)
+    methods.log_print("거래 대상 선정 시작")
+
+    # 내 보유 코인을 가져온다
+    my_coins = methods.get_my_coins(upbit)
+
+    # 매수 매도 로직 실행여부
+    sell_sign = False
+    buy_sign = False
+
+    # 보유 코인이 있는경우
+    if len(my_coins) > 0:
+        # 거래대상 코인 = 보유 코인
+        target_coins = my_coins
+        # 매도신호 on
+        sell_sign = True
+    # 보유 코인이 없는경우
+    else:
+        # 거래량 상위 코인 목록을 가져온다
+        target_coins = methods.get_top_coin_list("minute60", 10)
+    # 거래대상 코인 목록을 출력한다
     target_coins_str = ""
     for target in target_coins:
         if target_coins_str == "":
             target_coins_str += str(target)
         else:
             target_coins_str += (", " + str(target))
-    methods.log_print("투자 대상 선정 완료: " + target_coins_str)
+    methods.log_print("거래 대상 목록 선정 완료: " + target_coins_str)
 
-    for target_coin in target_coins:
-        methods.log_print("투자대상 단일화: " + str(target_coin))
-        # 투자 대상 거래 수수료
-        target_info = upbit.get_chance(target_coin)     # 투자 코인 시장정보
-        buy_fees = float(target_info["bid_fee"])        # 투자 코인 매수 수수료
-        sell_fees = float(target_info["ask_fee"])       # 투자 코인 매도 수수료
+    # 매도신호가 들어온 경우
+    if sell_sign:
+        # 타겟 코인들을 루프 돌면서
+        for target_coin in target_coins:
+            # 매도로직을 실행해준다.
+            sell_result = methods.sell_logic(upbit, target_coin, invest_balance, now_balance)
+            # 매도가 완료되었다면
+            if sell_result:
+                # 거래 대상 목록에서 매도한 코인을 제외한다.
+                target_coins.remove(target_coin)
 
-        while True:
-            # 하락장, 상승장, 일반장 구분
-            df_60 = pyupbit.get_ohlcv(target_coin, "minute60")  # 60분봉 정보
-            rsi_14_before = methods.get_rsi(df_60, 14, -4)      # 3시간 전 rsi14 지표
-            rsi_14_after = methods.get_rsi(df_60, 14, -2)       # 1시간 전 rsi14 지표
-            condition = rsi_14_after / rsi_14_before            # 1시간 전 지표 / 4시간 전 지표(장 상황 체크기준)
-            market_status = "normal"                            # 장 상태(일반장)
-            if condition >= 1.1:
-                market_status = "fire"                          # 장 상태(상승장) 체크기준이 1.1 이상인 경우
-            elif condition < 0.95:
-                market_status = "cold"                          # 장 상태(하락장) 체크기준이 0.95 이하인 경우
-            methods.log_print("시장 상황: " + market_status)
+    # 거래 대상 목록이 다 사라진 경우
+    if len(target_coins) == 0:
+        # 거래 대상 선정부터 다시한다.
+        continue
+    # 거래 대상이 남아있는 경우
+    else:
+        # 매수 신호를 보낸다.
+        buy_sign = True
 
-            # 상황별 분봉 정보
-            candle_interval = "minute5"     # 기본 분봉 5분봉
-            df = pyupbit.get_ohlcv(target_coin, candle_interval, 200)
-            candles = list()
-            # 양봉, 음봉 분기 및 시가 종가 데이터 담기
-            for i in range(-1, -21, -1):
-                if df["close"][i] - df["open"][i] >= 0:
-                    candle_type = "plus"
+    # 매수 신호가 들어온 경우
+    if buy_sign:
+        # 이미 구매한 코인이 있는경우
+        if sell_sign:
+            # 매수로직을 진행한다.
+            # 매수로직에는 매도로직이 함께한다
+            # 매수간 텀은 최소 5분이상 둔다.
+            # 매수로직은 대상체크 메소드와 구매 메소드 2개로 이루어진다.
+            # 보유한 코인을 모두 다 판매한 경우 매수로직이 종료된다.
+            methods.log_print("매수로직 짜야함")
+
+            # 보유한 코인을 모두 판매하고 나온경우
+            methods.log_print("보유한 코인을 모두 판매하였습니다. 봇을 다시 실행합니다.")
+        # 구매한 코인이 없는경우
+        else:
+            # 거래 대상들을 돌면서 구매할만한 코인이 있는지 확인한다.
+            for target_coin in target_coins:
+                # 구매대상 있는지 검사 로직
+                purchase_check = methods.check_purchase_target()
+                # 구매대상에 해당하는 경우
+                if purchase_check:
+                    # 해당 코인을 루프돌며 거래하는 로직으로 들어간다
+                    methods.print('매수로직 짜야행')
+                # 구매대상에 해당하지 않는 경우
                 else:
-                    candle_type = "minus"
-                temp = {
-                    "index": df.index[i],
-                    "candle_type": candle_type,
-                    "open": df["open"][i],
-                    "close": df["close"][i]
-                }
-                candles.append(temp)
-            now_candle = candles[0]
-            buy_sign = False
+                    continue
+            # 루프를 다 돌았으나 구매대상이 없는경우 => 다시 최초 로직으로
+            methods.log_print("구매에 적합한 대상이 없습니다. 봇을 다시 실행합니다.")
 
-            now_rsi_14 = methods.get_rsi(df, 14, -1)
-            comparison_rsi_14 = ""
-            if now_candle["candle_type"] == "minus":
-                buy_sign = False
-            else:
-                comparison_candle = ""
-                near_minus_candle = ""
-                near_minus_rsi_14 = float(0.0)
-                comparison_rsi_14 = float(0.0)
-                for i in range(1, len(candles)):
-                    if candles[i]["candle_type"] == "minus":
-                        if comparison_candle == "":
-                            comparison_candle = candles[i]
-                            near_minus_candle = candles[i]
-                            comparison_rsi_14 = methods.get_rsi(df, 14, (i * -1 - 1))
-                            near_minus_rsi_14 = methods.get_rsi(df, 14, (i * -1 - 1))
-                        else:
-                            if comparison_candle["open"] <= candles[i]["open"]:
-                                comparison_candle = candles[i]
-                                comparison_rsi_14 = methods.get_rsi(df, 14, (i * -1 - 1))
-                            else:
-                                break
+        # ------------------------------------------------------------------ 아래 참조해서 매수로직 다시짜자
 
-                if near_minus_rsi_14 > 0 and comparison_rsi_14 > 0 and now_rsi_14 > 0:
-                    if near_minus_rsi_14 / comparison_rsi_14 <= 0.75 and now_rsi_14 / near_minus_rsi_14 >= 1.1:
-                        buy_sign = True
+        # last_buy_time = time.time()
+        # while True:
+        #
+        #
+        #     # 상황별 분봉 정보
+        #     candle_interval = "minute5"     # 기본 분봉 5분봉
+        #     df = pyupbit.get_ohlcv(target_coin, candle_interval, 200)
+        #     candles = list()
+        #     # 양봉, 음봉 분기 및 시가 종가 데이터 담기
+        #     for i in range(-1, -21, -1):
+        #         if df["close"][i] - df["open"][i] >= 0:
+        #             candle_type = "plus"
+        #         else:
+        #             candle_type = "minus"
+        #         temp = {
+        #             "index": df.index[i],
+        #             "candle_type": candle_type,
+        #             "open": df["open"][i],
+        #             "close": df["close"][i]
+        #         }
+        #         candles.append(temp)
+        #     now_candle = candles[0]
+        #     buy_sign = False
+        #
+        #     now_rsi_14 = methods.get_rsi(df, 14, -1)
+        #     comparison_rsi_14 = ""
+        #     if now_candle["candle_type"] == "minus":
+        #         buy_sign = False
+        #     else:
+        #         comparison_candle = ""
+        #         near_minus_candle = ""
+        #         near_minus_rsi_14 = float(0.0)
+        #         comparison_rsi_14 = float(0.0)
+        #         for i in range(1, len(candles)):
+        #             if candles[i]["candle_type"] == "minus":
+        #                 if comparison_candle == "":
+        #                     comparison_candle = candles[i]
+        #                     near_minus_candle = candles[i]
+        #                     comparison_rsi_14 = methods.get_rsi(df, 14, (i * -1 - 1))
+        #                     near_minus_rsi_14 = methods.get_rsi(df, 14, (i * -1 - 1))
+        #                 else:
+        #                     if comparison_candle["open"] <= candles[i]["open"]:
+        #                         comparison_candle = candles[i]
+        #                         comparison_rsi_14 = methods.get_rsi(df, 14, (i * -1 - 1))
+        #                     else:
+        #                         break
+        #
+        #         if near_minus_rsi_14 > 0 and comparison_rsi_14 > 0 and now_rsi_14 > 0:
+        #             if near_minus_rsi_14 / comparison_rsi_14 <= 0.75 and now_rsi_14 / near_minus_rsi_14 >= 1.1:
+        #                 buy_sign = True
+        #
+        #     # 투자 대상 거래 수수료
+        #     target_info = upbit.get_chance(target_coin)     # 투자 코인 시장정보
+        #     buy_fees = float(target_info["bid_fee"])        # 투자 코인 매수 수수료
+        #     sell_fees = float(target_info["ask_fee"])       # 투자 코인 매도 수수료
+        #     # 매수로직
+        #     if buy_sign:
+        #         methods.log_print("매수로직을 시작합니다.")
+        #         # 1회당 매수금액 설정
+        #         budget_for_buy_once = invest_balance * 0.1                                          # 1회 매수 금액
+        #         now_my_balance = int(upbit.get_balance("KRW"))                                      # 현재 잔고
+        #         min_buy_price = float(target_info["market"]["bid"]["min_total"])                    # 최소 매수 금액
+        #
+        #         # 현재 잔고가 최소주문금액 이상인 경우에만 매수 로직 실행
+        #         if now_my_balance >= min_buy_price * (1 + buy_fees):
+        #             # 현재 잔고가 최소 주문금액 2회를 할 수 없는경우
+        #             if now_my_balance < math.ceil(min_buy_price * (1 + buy_fees)) * 2:
+        #                 budget_for_buy_once = math.floor(now_my_balance / (1 + buy_fees))           # 1회 매수 금액 = 현재 잔고 전액
+        #             else:
+        #                 budget_for_buy_once = math.floor(budget_for_buy_once / (1 + buy_fees))      # 수수료 공제포함 1회 주문금액
+        #                 # 1회 매수금액이 최소 주문금액보다 작은경우
+        #                 if budget_for_buy_once < min_buy_price:
+        #                     budget_for_buy_once = min_buy_price                                     # 1회 매수 금액 = 최소 주문 금액
+        #                 # 1회 매수를 한 이후 잔액이 최소 주문금액보다 작은경우
+        #                 if now_my_balance - (budget_for_buy_once * (1 + buy_fees)) < min_buy_price:
+        #                     budget_for_buy_once = math.floor(now_my_balance / (1 + buy_fees))       # 1회 매수 금액 = 수수료 공제 포함 현재 잔고 전액
+        #             buy_result = upbit.buy_market_order(target_coin, budget_for_buy_once)
+        #             methods.log_print("매수 성공: " + str(buy_result["price"]))
+        #             time.sleep(1)
+        #         else:
+        #             methods.log_print("잔액부족 매수실패")
+        #     else:
+        #         methods.log_print("시장 상황이 적합하지 않습니다. 매수로직을 건너뜁니다.")
 
-            # 매수로직
-            if buy_sign:
-                methods.log_print("매수로직을 시작합니다.")
-                # 1회당 매수금액 설정
-                budget_for_buy_once = invest_balance * 0.1                                          # 1회 매수 금액
-                now_my_balance = int(upbit.get_balance("KRW"))                                      # 현재 잔고
-                min_buy_price = float(target_info["market"]["bid"]["min_total"])                    # 최소 매수 금액
 
-                # 현재 잔고가 최소주문금액 이상인 경우에만 매수 로직 실행
-                if now_my_balance >= min_buy_price * (1 + buy_fees):
-                    # 현재 잔고가 최소 주문금액 2회를 할 수 없는경우
-                    if now_my_balance < math.ceil(min_buy_price * (1 + buy_fees)) * 2:
-                        budget_for_buy_once = math.floor(now_my_balance / (1 + buy_fees))           # 1회 매수 금액 = 현재 잔고 전액
-                    else:
-                        budget_for_buy_once = math.floor(budget_for_buy_once / (1 + buy_fees))      # 수수료 공제포함 1회 주문금액
-                        # 1회 매수금액이 최소 주문금액보다 작은경우
-                        if budget_for_buy_once < min_buy_price:
-                            budget_for_buy_once = min_buy_price                                     # 1회 매수 금액 = 최소 주문 금액
-                        # 1회 매수를 한 이후 잔액이 최소 주문금액보다 작은경우
-                        if now_my_balance - (budget_for_buy_once * (1 + buy_fees)) < min_buy_price:
-                            budget_for_buy_once = math.floor(now_my_balance / (1 + buy_fees))       # 1회 매수 금액 = 수수료 공제 포함 현재 잔고 전액
-                    buy_result = upbit.buy_market_order(target_coin, budget_for_buy_once)
-                    methods.log_print("매수 성공: " + str(buy_result["price"]))
-                    time.sleep(1)
-                else:
-                    methods.log_print("잔액부족 매수실패")
-            else:
-                methods.log_print("시장 상황이 적합하지 않습니다. 매수로직을 건너뜁니다.")
-
-            # 매도로직(대상 코인을 매수한 경우에만 진입)
-            if methods.is_has_coin(upbit.get_balances(), target_coin):
-                methods.log_print("매도로직을 시작합니다.")
-                is_break = False
-                for coin in upbit.get_balances():
-                    if "KRW-" + coin["currency"] != target_coin:
-                        continue
-                    # 목표 수익율 설정
-                    target_revenue = 1.005
-                    if market_status == "normal":
-                        target_revenue = 1.005
-                    elif market_status == "fire":
-                        target_revenue = 1.02
-                    elif market_status == "cold":
-                        target_revenue = 1.002
-
-                    target_price = float(coin["avg_buy_price"]) * target_revenue
-                    if pyupbit.get_current_price(target_coin) >= target_price:
-                        upbit.sell_market_order(coin, float(coin["balance"]))
-                        methods.log_print("코인을 모두 판매했습니다. 10초 뒤 대상 선정을 다시 시작합니다.")
-                        is_break = True
-                    else:
-                        methods.log_print("수익률에 도달하지 못했습니다. 10초 뒤 시장상황 분석 로직을 다시 시작합니다.")
-            else:
-                methods.log_print("코인을 구매하지 않았습니다. 10초 뒤 대상 선정을 다시 시작합니다.")
-                is_break = True
-            time.sleep(10)
-            if is_break:
-                break

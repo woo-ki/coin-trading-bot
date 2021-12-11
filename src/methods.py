a02628f0ee4c76009b37d633fa7f4967a5ff8508
김영욱 <ziziza93@naver.com>
2021-12-11 15:48:33 +0900

    매수로직 추가

--- src/methods.py
+++ src/methods.py
@@ -237,10 +237,15 @@ def sell_logic(upbit, target_coin, invest_balance, except_balance, custom_revenu
 
     real_avg_buy_price = get_real_avg_buy_price(upbit, target_coin, invest_balance, except_balance)
 
-    # 목표 가격을 정하고 목표 가격보다 현재 가격이 높은경우 매도한다.
+    # 목표 가격을 정하고 목표 가격보다 현재 가격이 높고, 코인의 매도세가 시작된 경우 매도한다.
     target_price = real_avg_buy_price * target_revenue
     now_price = pyupbit.get_current_price(target_coin)
-    if now_price >= target_price:
+    # 1분봉 기준 rsi지표 생성
+    df_1 = pyupbit.get_ohlcv(target_coin, "minute1")
+    before_rsi_1 = get_rsi(df_1, 14, -1)
+    now_rsi_1 = get_rsi(df_1, 14, -2)
+    is_rsi_1_degradation = before_rsi_1 / now_rsi_1 < 0.97
+    if now_price >= target_price and is_rsi_1_degradation:
         upbit.sell_market_order(target_coin, upbit.get_balance(target_coin))
         log_print(str(target_coin) + " 코인을 모두 판매했습니다.")
         delay_for_deal_api()
